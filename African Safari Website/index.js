3481f7ee1e87c80c06b515cb79d8b1fedfc8b9bb
SourceGlitch <134461243+SourceGlitch@users.noreply.github.com>
2025-03-16 18:04:40 -0400

    "Changes were made"

---
+++ African Safari Website/index.js
@@ -0,0 +1,11 @@
+// show Sidebar
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+}
+
+//hide Sidebar
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "none";
+}
